ee3bcc71623278f1d28680c41ecc53c20bd75ce2
mireleschavezv <mireleschavezv@semantic-web.com>
2018-02-28 23:14:31 +0100

    turned into module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='tt_light',
     version='0.1dev',
-    packages=['tt_light'],
+    packages=['light_topic_transition'],
     license='MIT',
     requires=open('requirements.txt', 'r').read().split()
 )
